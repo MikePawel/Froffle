c207ff418673b53de82d3a4179998ecae691892d
santhosh <santosh.kumar@zenotta.com>
2024-07-12 22:05:43 +0200

    wallet connect support

--- Frontend/src/components/Display/Display.tsx
+++ Frontend/src/components/Display/Display.tsx
@@ -1,23 +1,8 @@
-import { useMetaMask } from '~/hooks/useMetaMask'
-import { formatChainAsNum } from '~/utils'
-import styles from './Display.module.css'
-import { Link } from "react-router-dom"
-
 export const Display = () => {
-
-  const { wallet } = useMetaMask()
-
   return (
-    <div className={styles.display}>
-      {wallet.accounts.length > 0 &&
-        <>
-          <div>Wallet Accounts: {wallet.accounts[0]}</div>
-          <div>Wallet Balance: {wallet.balance}</div>
-          <div>Hex ChainId: {wallet.chainId}</div>
-          <div>Numeric ChainId: {formatChainAsNum(wallet.chainId)}</div>
-        </>
-      }
-      <Link to="/test">Go to testpage</Link>
+    <div className="Display">
+      <h1>W3M Button</h1>
+      <w3m-button />
     </div>
-  )
-}
+  );
+};
